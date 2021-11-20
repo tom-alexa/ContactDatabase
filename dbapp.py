2ac82f39d99fcf1f257ebe3b0f63eb638ec2a371
Tom Alexa <tom.alexa.fin@gmail.com>
2021-11-20 14:39:25 +0100

    Create database tables

--- dbapp.py
+++ dbapp.py
@@ -43,12 +43,49 @@ class ContactDatabase:
         self.db_path = Path(f"{Path(__file__).parent.resolve()}/database/{DB_NAME}")
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
-
-    def create_database(self):
-        pass
+        self.create_database()
 
     def select(self, table, parameters):
         pass
+
+    def create_database(self):
+        """
+        Create database tables if not already exists
+        """
+        tables = [
+            """CREATE TABLE IF NOT EXISTS 'contact_group' (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(255) NOT NULL UNIQUE)
+                ;""",
+            """CREATE TABLE IF NOT EXISTS 'contact' (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name VARCHAR(60),
+                last_name VARCHAR(60),
+                date_of_birth DATE,
+                group_id INTEGER,
+                street VARCHAR(60),
+                number_of_descriptive INTEGER,
+                city VARCHAR(60),
+                FOREIGN KEY (group_id)
+                    REFERENCES contact_group(id)
+                );""",
+            """CREATE TABLE IF NOT EXISTS 'prefix' (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                prefix INTEGER NOT NULL UNIQUE,
+                state VARCHAR(255) NOT NULL UNIQUE
+                );""",
+            """CREATE TABLE IF NOT EXISTS 'phone_number' (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                prefix_id INTEGER NOT NULL DEFAULT 420,
+                number INTEGER NOT NULL, contact_id INTEGER,
+                FOREIGN KEY (prefix_id)
+                    REFERENCES prefix(id)
+                );""",
+        ]
+        self.cursor.execute("PRAGMA foreign_keys = ON;")
+        for table in tables:
+            self.cursor.execute(table)
+        self.connection.commit()
 
 
 def main():
