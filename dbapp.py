13752d1861b2a5d9eb704059d43cc4ee7e7a1f9e
Tom Alexa <tom.alexa.fin@gmail.com>
2021-11-20 15:22:24 +0100

    Add main running loop

--- dbapp.py
+++ dbapp.py
@@ -4,7 +4,6 @@
 
 
 import sqlite3
-import os
 from pathlib import Path
 
 ##############
@@ -16,17 +15,49 @@ DB_NAME = "contacts.db"
 
 
 class App:
-    def __init__(self):
-        self.db = ContactDatabase()
+    TO_PRINT = {
+        "input": {
+            "en": "Choose one option: ",
+            "cz": "Vyber jednu z možností: ",
+        },
+    }
+
+    def __init__(self, language="cz"):
+        self._language = language
+        self._db = ContactDatabase()
+        self.running = True
 
     def run(self):
-        pass
+        """
+        Main running loop
+        """
+        while self.running:
+            option, parameters = self.get_option()
+            self.manage_option(option, parameters)
+        self.close()
 
     def get_option(self):
-        pass
+        """
+        Input format: option parameter parameter, ex. l -t contact
+        Return: option, parameters (ex. 'l', ['-t', 'contact'])
+        """
+        while True:
+            command = list(filter(None, input(self.TO_PRINT["input"][self._language]).strip().split(" ")))
+            if not command:
+                continue
+            option = command[0].upper()
+            parameters = list(map(lambda c: c.strip(), command[1:]))
+            return option, parameters
 
-    def manage_option(self, option):
-        pass
+    def manage_option(self, option, parameters):
+        if option == "Q":
+            self.running = False
+        elif option == "L":
+            pass
+        elif option == "I":
+            pass
+        elif option == "D":
+            pass
 
     def show(parameters):
         pass
@@ -36,6 +67,9 @@ class App:
 
     def delete(parameters):
         pass
+
+    def close(self):
+        self._db.close()
 
 
 class ContactDatabase:
@@ -56,7 +90,8 @@ class ContactDatabase:
             """CREATE TABLE IF NOT EXISTS 'contact_group' (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name VARCHAR(255) NOT NULL UNIQUE)
-                ;""",
+                ;
+            """,
             """CREATE TABLE IF NOT EXISTS 'contact' (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 first_name VARCHAR(60),
@@ -68,24 +103,30 @@ class ContactDatabase:
                 city VARCHAR(60),
                 FOREIGN KEY (group_id)
                     REFERENCES contact_group(id)
-                );""",
+                );
+            """,
             """CREATE TABLE IF NOT EXISTS 'prefix' (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 prefix INTEGER NOT NULL UNIQUE,
                 state VARCHAR(255) NOT NULL UNIQUE
-                );""",
+                );
+            """,
             """CREATE TABLE IF NOT EXISTS 'phone_number' (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 prefix_id INTEGER NOT NULL DEFAULT 420,
                 number INTEGER NOT NULL, contact_id INTEGER,
                 FOREIGN KEY (prefix_id)
                     REFERENCES prefix(id)
-                );""",
+                );
+            """,
         ]
         self.cursor.execute("PRAGMA foreign_keys = ON;")
         for table in tables:
             self.cursor.execute(table)
         self.connection.commit()
+
+    def close(self):
+        self.connection.close()
 
 
 def main():
